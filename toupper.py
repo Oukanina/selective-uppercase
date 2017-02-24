ecfa38a8264f6b66840263a7485a2c50c7b2bd0d
guokeke <guo.ke.ke.a@gmail.com>
2017-02-24 12:48:45 +0800

    Add keywords

--- toupper.py
+++ toupper.py
@@ -1,8 +1,10 @@
 import sublime
 import sublime_plugin
 import re
+import json
 
 
+CONFIG_FILE_PATH = 'Packages/toupper/keywords.json'
 word_match = re.compile(r"(\W|\w+)")
 
 
@@ -12,20 +14,22 @@ class ToupperCommand(sublime_plugin.TextCommand):
         window = sublime.active_window()
         view = window.active_view()
         sel = view.sel()
+        self.keywords_list = json.loads(
+            sublime.load_resource(CONFIG_FILE_PATH))['list']
         for region in sel:
             self.convert_one_by_one(edit, region)
 
     def convert_one_by_one(self, edit, region):
         begin = region.begin()
         end = region.end()
-        word_match = re.compile(r"(\W|\w+)")
         lines = self.view.substr(region).split('\n')
         for line_index, line in enumerate(lines):
             words = word_match.findall(line)
             for word_index, word in enumerate(words):
-                words[word_index] = word.upper()
+                if self.is_keyword(word):
+                    words[word_index] = word.upper()
             lines[line_index] = ''.join(words)
         self.view.replace(edit, region, '\n'.join(lines))
 
-    # def iskeyword(self, word):
-    #     return word.upper() in keywords_list
+    def is_keyword(self, word):
+        return word.upper() in self.keywords_list or word.lower() in self.keywords_list
